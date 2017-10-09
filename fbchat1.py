d1994e7424a92a8b32e87d0b783e4fcb1dcde6bf
Chatchai Saratakij <CSaratakij@gmail.com>
2017-10-09 18:03:31 +0700

    Improve readability.

--- fbchat1.py
+++ fbchat1.py
@@ -1,13 +1,17 @@
 import fbchat
+
 from fbchat import Client
 from fbchat.models import *
-def login(username,password):
-	return fbchat.Client(username,password, max_tries=1)
+
+def login(username, password):
+	return fbchat.Client(username, password, max_tries = 1)
+
 def iLI(client):
 	return client.isLoggedIn()
-def sendmsg(client,msg,name):
-	friends=client.searchForUsers(name)
-	friend=friends[0]
-	sent=client.sendMessage(msg,thread_id=friend.uid, thread_type=ThreadType.USER)
+
+def send_msg(client, msg,name):
+	friends = client.searchForUsers(name)
+	friend = friends[0]
+	sent = client.sendMessage(msg, thread_id = friend.uid, thread_type=ThreadType.USER)
 	if sent:
 		print("Message sent!")
